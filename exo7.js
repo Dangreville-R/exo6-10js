f771b65e5ef8799e87d1f6530715942589d452ae
Dangreville-R <rdangreville@gmail.com>
2024-11-27 10:22:50 +0100

    exo7finito

--- exo7.js
+++ exo7.js
@@ -1,12 +1,5 @@
-//on fait une variable factorielle egale a 0
-let factorielle = 0;
-//on demande un nombre qu'on et dans la variable nbrDemande
-let nbrDemande = prompt("entrer un nombre :");
-var nbr = 1
-for(i=0;i<nbrDemande;i++) {
-   factorielle = factorielle + nbr;
-   nbr = nbr +1;
-   alert(factorielle);
+function addition(param1,param2)
+{
+   var resultat = param1+param2;
+   return resultat;
 }
-factorielle = factorielle - nbrDemande;
-alert(factorielle);
